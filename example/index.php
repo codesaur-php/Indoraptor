d7b59c9c16ca392d8e59d12be46d02c9f9a1b26f
Наранхүү Н <codesaur@gmail.com>
2021-10-06 20:50:31 +0800

    Update index.php

--- example/index.php
+++ example/index.php
@@ -9,7 +9,7 @@ namespace Indoraptor\Example;
 
 define('CODESAUR_DEVELOPMENT', true);
 
-ini_set('display_errors', 'Off');
+ini_set('display_errors', 'On');
 error_reporting(E_ALL & ~E_STRICT & ~E_NOTICE);
 
 use Indoraptor\IndoApplication;
