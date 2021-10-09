f2d6827b59d383bef866eef4f6f2f6dc677a4169
Наранхүү Н <codesaur@gmail.com>
2021-10-10 04:33:23 +0800

    adds codesaur\Http\Client\Mail

--- src/mail/EmailController.php
+++ src/mail/EmailController.php
@@ -5,8 +5,7 @@ namespace Indoraptor\Mail;
 use Throwable;
 use Exception;
 
-use PHPMailer\PHPMailer\PHPMailer;
-
+use codesaur\Http\Client\Mail;
 use codesaur\Localization\TranslationModel;
 
 class EmailController extends \Indoraptor\IndoController
@@ -31,7 +30,7 @@ class EmailController extends \Indoraptor\IndoController
                 throw new Exception($text['emailer-not-set'] ?? 'Email sender not found!');
             }
             
-            $this->sendBasic(getenv('MAIL_SENDER', true), $payload['to'], $payload['subject'], $payload['message']);
+            (new Mail())->send(getenv('MAIL_SENDER', true), $payload['to'], $payload['subject'], $payload['message']);
             // TODO: email-succesfully-sent iig translation deer nemeh
             $this->respond(array('success' => array('message' => $text['email-succesfully-sent'] ?? 'Email successfully sent to destination')));
         } catch (Throwable $th) {
@@ -65,70 +64,16 @@ class EmailController extends \Indoraptor\IndoController
                     || !isset($record['is_smtp']) || !isset($record['smtp_auth']) || !isset($record['smtp_secure'])) {
                 throw new Exception($text['emailer-not-set'] ?? 'Email carrier not found!');
             }
-
-            $mailer = new PHPMailer($this->isDevelopment() ? true : null);
-            if (((int)$record['is_smtp']) == 1) {
-               $mailer->IsSMTP(); 
-            }
-            $mailer->Mailer = 'smtp';
-            $mailer->CharSet = $record['charset'];
-            $mailer->SMTPAuth = (bool)((int)$record['smtp_auth']);
-            $mailer->SMTPSecure = $record['smtp_secure'];
-            $mailer->Host = $record['host'];
-            $mailer->Port = $record['port'];
-            $mailer->Username = $record['username'];
-            $mailer->Password = $record['password'];
-            $mailer->SetFrom($record['email'], $record['name']);
-            $mailer->AddReplyTo($record['email'], $record['name']);
-            $mailer->SMTPOptions = array('ssl' => array(
-                'verify_peer' => false, 'verify_peer_name' => false, 'allow_self_signed' => true));
-
-            $mailer->MsgHTML($payload['message']);
-            $mailer->Subject = $payload['subject'];
-            $mailer->AddAddress($payload['to'], $payload['name'] ?? '');
-            $mailer->Send();
+            
+            (new Mail())->sendSMTP(
+                    $record['email'], $record['name'], $payload['to'], $payload['name'] ?? '',
+                    $payload['subject'], $payload['message'], $record['charset'],
+                    $record['host'], $record['port'], $record['username'], $record['password'],
+                    ((int)$record['is_smtp']) == 1, (bool)((int)$record['smtp_auth']), $record['smtp_secure']);
             
             $this->respond(array('success' => array('message' => $text['email-succesfully-sent'] ?? 'Email successfully sent to destination')));
         } catch (Throwable $th) {
             $this->error($th->getMessage(), $th->getCode());
-        }
-    }
-    
-    public function sendBasic($from, $to, $subject, $message)
-    {
-        if (!isset($from)) {
-            throw new Exception('Mail sender must be set!');
-        } elseif (is_array($from)) {
-            $sender = '=?UTF-8?B?' . base64_encode($from[0]) . '?= <' . $from[1] . '>';
-        }
-  
-        if (!isset($to)) {
-            throw new Exception('Mail recipient must be set!');
-        } elseif (is_array($to)) {
-            $recipient = '=?UTF-8?B?' . base64_encode($to[0]) . '?= <' . $to[1] . '>';
-        }
-        
-        if (empty($subject) || empty($message)) {
-            throw new Exception('No content? Are u kidding? Mail message must be set!');
-        }
-        
-        $content_type = strpos($message, '</') === false ? 'text/plain' : 'text/html';
-        
-        $subject = '=?UTF-8?B?' . base64_encode($subject) . '?=';
-        
-        $header  = 'MIME-Version: 1.0' . "\r\n";
-        $header .= 'Content-type: ' . $content_type . '; charset=utf-8' . "\r\n";
-        $header .= 'Content-Transfer-Encoding: base64' . "\r\n";
-        $header .= 'Date: ' . date('r (T)') . "\r\n";
-        
-        $from_addr =  $from ?? $sender;
-        $header .= 'From: ' . $from_addr . "\r\n";
-        $header .= 'Reply-To: ' . $from_addr . "\r\n";
-        
-        $header .= 'X-Mailer: PHP/' . phpversion();
-        
-        if (!mail($recipient ?? $to, $subject, base64_encode($message), $header)) {
-            throw new Exception(error_get_last()['message'] ?? 'Email not sent!');
         }
     }
 }
