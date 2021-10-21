ad4859ec07f51dc9234b6cfd3827ddc420a0a436
Наранхүү Н <codesaur@gmail.com>
2021-10-21 17:58:35 +0800

    Update DashboardMenuModel.php

--- src/contents/DashboardMenuModel.php
+++ src/contents/DashboardMenuModel.php
@@ -16,7 +16,7 @@ class DashboardMenuModel extends MultiModel
         $this->setColumns(array(
            (new Column('id', 'bigint', 20))->auto()->primary()->unique()->notNull(),
             new Column('parent_id', 'int', 20, 0),
-            new Column('feather', 'varchar', 64),
+            new Column('icon', 'varchar', 64),
             new Column('href', 'varchar', 1024),
             new Column('position', 'int', 8, 100),
             new Column('is_active', 'tinyint', 1, 1),
@@ -48,17 +48,17 @@ class DashboardMenuModel extends MultiModel
         }
         
         $idMain = $this->insert(array('position' => '10'), array('mn' => array('title' => 'Үндсэн'), 'en' => array('title' => 'Main')));
-        $this->insert(array('parent_id' => $idMain, 'position' => '11', 'feather' => 'home', 'href' => '/'), array('mn' => array('title' => 'Нүүр хуудасруу зочлох'), 'en' => array('title' => 'Go to homepage')));
+        $this->insert(array('parent_id' => $idMain, 'position' => '11', 'icon' => 'home', 'href' => '/'), array('mn' => array('title' => 'Нүүр хуудасруу зочлох'), 'en' => array('title' => 'Go to homepage')));
 
         $idContent = $this->insert(array('position' => '200'), array('mn' => array('title' => 'Агуулгууд'), 'en' => array('title' => 'Contents')));
-        $this->insert(array('parent_id' => $idContent, 'position' => '210', 'feather' => 'flag', 'href' => '/language'), array('mn' => array('title' => 'Хэл'), 'en' => array('title' => 'Language')));
-        $this->insert(array('parent_id' => $idContent, 'position' => '220', 'feather' => 'globe', 'href' => '/translation'), array('mn' => array('title' => 'Орчуулга'), 'en' => array('title' => 'Translation')));
-        $this->insert(array('parent_id' => $idContent, 'position' => '230', 'feather' => 'archive', 'href' => '/templates'), array('mn' => array('title' => 'Баримт бичгийн загварууд'), 'en' => array('title' => 'Document Templates')));
+        $this->insert(array('parent_id' => $idContent, 'position' => '210', 'icon' => 'flag', 'href' => '/language'), array('mn' => array('title' => 'Хэл'), 'en' => array('title' => 'Language')));
+        $this->insert(array('parent_id' => $idContent, 'position' => '220', 'icon' => 'globe', 'href' => '/translation'), array('mn' => array('title' => 'Орчуулга'), 'en' => array('title' => 'Translation')));
+        $this->insert(array('parent_id' => $idContent, 'position' => '230', 'icon' => 'archive', 'href' => '/templates'), array('mn' => array('title' => 'Баримт бичгийн загварууд'), 'en' => array('title' => 'Document Templates')));
 
         $idSystem = $this->insert(array('position' => '300'), array('mn' => array('title' => 'Систем'), 'en' => array('title' => 'System')));
-        $this->insert(array('parent_id' => $idSystem, 'position' => '310', 'feather' => 'users', 'href' => '/accounts'), array('mn' => array('title' => 'Хэрэглэгчид'), 'en' => array('title' => 'Accounts')));
-        $this->insert(array('parent_id' => $idSystem, 'position' => '320', 'feather' => 'list', 'href' => '/organizations'), array('mn' => array('title' => 'Байгууллага'), 'en' => array('title' => 'Organization')));
-        $this->insert(array('parent_id' => $idSystem, 'position' => '330', 'feather' => 'list', 'href' => '/mailer'), array('mn' => array('title' => 'Шууданч'), 'en' => array('title' => 'Mail carrier')));
-        $this->insert(array('parent_id' => $idSystem, 'position' => '340', 'feather' => 'list', 'href' => '/log'), array('mn' => array('title' => 'Хандалтын протокол'), 'en' => array('title' => 'Access log')));
+        $this->insert(array('parent_id' => $idSystem, 'position' => '310', 'icon' => 'users', 'href' => '/accounts'), array('mn' => array('title' => 'Хэрэглэгчид'), 'en' => array('title' => 'Accounts')));
+        $this->insert(array('parent_id' => $idSystem, 'position' => '320', 'icon' => 'list', 'href' => '/organizations'), array('mn' => array('title' => 'Байгууллага'), 'en' => array('title' => 'Organization')));
+        $this->insert(array('parent_id' => $idSystem, 'position' => '330', 'icon' => 'list', 'href' => '/mailer'), array('mn' => array('title' => 'Шууданч'), 'en' => array('title' => 'Mail carrier')));
+        $this->insert(array('parent_id' => $idSystem, 'position' => '340', 'icon' => 'list', 'href' => '/log'), array('mn' => array('title' => 'Хандалтын протокол'), 'en' => array('title' => 'Access log')));
     }
 }
