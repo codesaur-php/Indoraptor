16408da9f5c289105f21e3b1a231f10b3a480b1d
Наранхүү Н <codesaur@gmail.com>
2021-10-21 16:46:55 +0800

    Update DashboardMenuModel.php

--- src/contents/DashboardMenuModel.php
+++ src/contents/DashboardMenuModel.php
@@ -42,5 +42,23 @@ class DashboardMenuModel extends MultiModel
         $this->exec("ALTER TABLE $table ADD CONSTRAINT {$table}_fk_created_by FOREIGN KEY (created_by) REFERENCES rbac_accounts(id) ON DELETE SET NULL ON UPDATE CASCADE");
         $this->exec("ALTER TABLE $table ADD CONSTRAINT {$table}_fk_updated_by FOREIGN KEY (updated_by) REFERENCES rbac_accounts(id) ON DELETE SET NULL ON UPDATE CASCADE");
         $this->setForeignKeyChecks(true);
+        
+        if ($table != 'dashboard_menu') {
+            return;
+        }
+        
+        $idMain = $this->insert(array('position' => '10'), array('mn' => array('title' => 'Үндсэн'), 'en' => array('title' => 'Main')));
+        $this->insert(array('parent_id' => $idMain, 'position' => '11', 'feather' => 'home', 'href' => '/'), array('mn' => array('title' => 'Нүүр хуудасруу зочлох'), 'en' => array('title' => 'Go to homepage')));
+
+        $idContent = $this->insert(array('position' => '200'), array('mn' => array('title' => 'Агуулгууд'), 'en' => array('title' => 'Contents')));
+        $this->insert(array('parent_id' => $idContent, 'position' => '210', 'feather' => 'flag', 'href' => '/language'), array('mn' => array('title' => 'Хэл'), 'en' => array('title' => 'Language')));
+        $this->insert(array('parent_id' => $idContent, 'position' => '220', 'feather' => 'globe', 'href' => '/translation'), array('mn' => array('title' => 'Орчуулга'), 'en' => array('title' => 'Translation')));
+        $this->insert(array('parent_id' => $idContent, 'position' => '230', 'feather' => 'archive', 'href' => '/templates'), array('mn' => array('title' => 'Баримт бичгийн загварууд'), 'en' => array('title' => 'Document Templates')));
+
+        $idSystem = $this->insert(array('position' => '300'), array('mn' => array('title' => 'Систем'), 'en' => array('title' => 'System')));
+        $this->insert(array('parent_id' => $idSystem, 'position' => '310', 'feather' => 'users', 'href' => '/accounts'), array('mn' => array('title' => 'Хэрэглэгчид'), 'en' => array('title' => 'Accounts')));
+        $this->insert(array('parent_id' => $idSystem, 'position' => '320', 'feather' => 'list', 'href' => '/organizations'), array('mn' => array('title' => 'Байгууллага'), 'en' => array('title' => 'Organization')));
+        $this->insert(array('parent_id' => $idSystem, 'position' => '330', 'feather' => 'list', 'href' => '/mailer'), array('mn' => array('title' => 'Шууданч'), 'en' => array('title' => 'Mail carrier')));
+        $this->insert(array('parent_id' => $idSystem, 'position' => '340', 'feather' => 'list', 'href' => '/log'), array('mn' => array('title' => 'Хандалтын протокол'), 'en' => array('title' => 'Access log')));
     }
 }
