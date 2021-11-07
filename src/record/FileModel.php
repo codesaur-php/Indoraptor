0b5b45d94123c2a4db3ed85371795dfb3df3204d
Наранхүү Н <codesaur@gmail.com>
2021-11-07 18:17:54 +0800

    added File models

---
+++ src/record/FileModel.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace Indoraptor\Record;
+
+use PDO;
+
+use codesaur\DataObject\Column;
+use codesaur\DataObject\MultiModel;
+
+class FileModel extends MultiModel
+{
+    function __construct(PDO $pdo)
+    {
+        parent::__construct($pdo);
+        
+        $this->setColumns(array(
+           (new Column('id', 'bigint', 20))->auto()->primary()->unique()->notNull(),
+            new Column('file', 'varchar', 255),
+            new Column('path', 'varchar', 255, ''),
+           (new Column('protection', 'tinyint', 1, 1))->notNull(), // 1 => public; 2 => private
+            new Column('category', 'int', 4, 1),
+            new Column('size', 'bigint', 20),
+            new Column('keyword', 'varchar', 128),
+            new Column('is_active', 'tinyint', 1, 1),
+            new Column('created_at', 'datetime'),
+            new Column('created_by', 'bigint', 20),
+            new Column('updated_at', 'datetime'),
+            new Column('updated_by', 'bigint', 20)
+        ));
+        
+        $this->setContentColumns(array(new Column('title', 'varchar', 1024)));
+        
+        $this->setTable('file');
+    }
+    
+    function __initial()
+    {
+        parent::__initial();
+        
+        $table = $this->getName();
+        
+        $this->setForeignKeyChecks(false);
+        $this->exec("ALTER TABLE $table ADD CONSTRAINT {$table}_fk_created_by FOREIGN KEY (created_by) REFERENCES rbac_accounts(id) ON DELETE SET NULL ON UPDATE CASCADE");
+        $this->exec("ALTER TABLE $table ADD CONSTRAINT {$table}_fk_updated_by FOREIGN KEY (updated_by) REFERENCES rbac_accounts(id) ON DELETE SET NULL ON UPDATE CASCADE");
+        $this->setForeignKeyChecks(true);
+    }
+    
+    public function getTableRecord(string $table, int $record, int $type, $code = null)
+    {
+        $files = new FilesModel($this->pdo);
+        $files->setTable($table);
+        
+        $condition = "record=$record AND type=$type AND is_active=1";
+        if (isset($code)) {
+            $condition .= " AND code='$code'";
+        }
+        $rows = $files->getRows(
+                array(
+                    'WHERE'    => $condition,
+                    'ORDER BY' => 'id desc',
+                    'LIMIT'    => 1
+                )
+        );
+        
+        $files_record = end($rows);
+        if (isset($files_record['file'])) {
+            $data = $this->getById($files_record['file'], $code);
+
+            if ($data) {
+                $data['files_id'] = $files_record['id'];
+                $data['record']   = $files_record['record'];
+                $data['type']     = $files_record['type'] ?? null;
+                $data['code']     = $files_record['code'] ?? null;
+                $data['rank']     = $files_record['rank'] ?? null;
+            }
+            
+            return $data;
+        }
+        
+        return null;
+    }
+}
---
+++ src/record/FilesModel.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace Indoraptor\Record;
+
+use PDO;
+
+use codesaur\DataObject\Model;
+use codesaur\DataObject\Column;
+
+class FilesModel extends Model
+{
+    function __construct(PDO $pdo)
+    {
+        parent::__construct($pdo);
+        
+        $this->setColumns(array(
+           (new Column('id', 'bigint', 20))->auto()->primary()->unique()->notNull(),
+           (new Column('record', 'bigint', 20))->notNull(),
+           (new Column('file', 'bigint', 20))->notNull(),
+            new Column('type', 'int', 5),
+            new Column('code', 'varchar', 6, ''),
+            new Column('rank', 'int', 8, 10),
+            new Column('is_active', 'tinyint', 1, 1),
+            new Column('created_at', 'datetime'),
+            new Column('created_by', 'bigint', 20),
+            new Column('updated_at', 'datetime'),
+            new Column('updated_by', 'bigint', 20)
+        ));
+        
+        $this->setTable('file');
+    }
+    
+    function __initial()
+    {
+        parent::__initial();
+        
+        $my_name = $this->getName();
+        $record_table_name = $this->getNameClean();
+        
+        $this->setForeignKeyChecks(false);
+        $this->exec("ALTER TABLE $my_name ADD CONSTRAINT {$my_name}_fk_file FOREIGN KEY (file) REFERENCES file(id) ON DELETE CASCADE ON UPDATE CASCADE");
+        $this->exec("ALTER TABLE $my_name ADD CONSTRAINT {$my_name}_fk_record FOREIGN KEY (record) REFERENCES $record_table_name(id) ON DELETE SET NULL ON UPDATE CASCADE");
+        $this->exec("ALTER TABLE $my_name ADD CONSTRAINT {$my_name}_fk_created_by FOREIGN KEY (created_by) REFERENCES rbac_accounts(id) ON DELETE SET NULL ON UPDATE CASCADE");
+        $this->exec("ALTER TABLE $my_name ADD CONSTRAINT {$my_name}_fk_updated_by FOREIGN KEY (updated_by) REFERENCES rbac_accounts(id) ON DELETE SET NULL ON UPDATE CASCADE");
+        $this->setForeignKeyChecks(true);
+    }
+    
+    public function setTable(string $name) : bool
+    {
+        return parent::setTable(preg_replace('/[^A-Za-z0-9_-]/', '', $name) . '_files');
+    }
+
+    public function getNameClean()
+    {
+        return substr($this->getTable(), 0, -(strlen('_files')));
+    }
+}
