31b92becbf232a531284684fa2ca8700d283b99c
Наранхүү <codesaur@gmail.com>
2022-11-20 17:36:25 +0800

    Update EmailRouter.php

--- src/mail/EmailRouter.php
+++ src/mail/EmailRouter.php
@@ -9,6 +9,6 @@ class EmailRouter extends Router
     function __construct()
     {
         $this->INTERNAL('/send/email', [EmailController::class, 'send']);
-        $this->INTERNAL('/send/stmp/email', [EmailController::class, 'sendSMTP']);
+        $this->INTERNAL('/send/smtp/email', [EmailController::class, 'sendSMTP']);
     }
 }
