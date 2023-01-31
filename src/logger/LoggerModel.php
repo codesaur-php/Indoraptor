b245f827a488fa91915def29fae86261427e77e8
Наранхүү <codesaur@gmail.com>
2023-01-31 23:49:35 +0800

    Update LoggerModel.php

--- src/logger/LoggerModel.php
+++ src/logger/LoggerModel.php
@@ -16,8 +16,6 @@ class LoggerModel extends \codesaur\Logger\Logger
     
     protected function __initial()
     {
-        parent::__initial();
-        
         $this->setForeignKeyChecks(false);
         $this->exec("ALTER TABLE {$this->getName()} ADD CONSTRAINT {$this->getName()}_fk_created_by FOREIGN KEY (created_by) REFERENCES rbac_accounts(id) ON DELETE SET NULL ON UPDATE CASCADE");
         $this->setForeignKeyChecks(true);
