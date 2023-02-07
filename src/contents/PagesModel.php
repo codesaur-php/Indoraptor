3a7b2b37404a5c5b38a1df846905c3c033696d39
Наранхүү <codesaur@gmail.com>
2023-02-07 14:50:48 +0800

    Create PagesModel.php

---
+++ src/contents/PagesModel.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace Indoraptor\Contents;
+
+use codesaur\DataObject\Column;
+use codesaur\DataObject\MultiModel;
+
+class PagesModel extends MultiModel
+{
+    public function __construct(\PDO $pdo)
+    {
+        $this->setInstance($pdo);
+        
+        $this->setColumns([
+           (new Column('id', 'bigint', 8))->auto()->primary()->unique()->notNull(),
+            new Column('parent_id', 'bigint', 8),
+            new Column('meta_id', 'bigint', 8),
+            new Column('category', 'varchar', 32, 'general'),
+            new Column('type', 'varchar', 32, 'menu'),
+            new Column('position', 'smallint', 2, 100),
+            new Column('published_at', 'datetime'),
+            new Column('link', 'varchar', 255),
+           (new Column('name', 'varchar', 128))->unique(),
+            new Column('show_comment', 'tinyint', 1, 0),
+            new Column('can_comment', 'tinyint', 1, 0),
+            new Column('read_count', 'int', 4, 0),
+            new Column('is_active', 'tinyint', 1, 1),
+            new Column('created_at', 'datetime'),
+            new Column('created_by', 'bigint', 8),
+            new Column('updated_at', 'datetime'),
+            new Column('updated_by', 'bigint', 8)
+        ]);
+        
+        $this->setContentColumns([
+            new Column('title', 'varchar', 255),
+            new Column('short', 'text'),
+            new Column('full', 'text'),
+            new Column('is_visible', 'tinyint', 1, 1)
+        ]);
+        
+        $this->setTable('pages', $_ENV['INDO_DB_COLLATION'] ?? 'utf8_unicode_ci');
+    }
+    
+    protected function __initial()
+    {
+        $this->setForeignKeyChecks(false);
+
+        $table = $this->getName();
+        $this->exec("ALTER TABLE $table ADD CONSTRAINT {$table}_fk_created_by FOREIGN KEY (created_by) REFERENCES rbac_accounts(id) ON DELETE SET NULL ON UPDATE CASCADE");
+        $this->exec("ALTER TABLE $table ADD CONSTRAINT {$table}_fk_updated_by FOREIGN KEY (updated_by) REFERENCES rbac_accounts(id) ON DELETE SET NULL ON UPDATE CASCADE");
+        
+        $this->setForeignKeyChecks(true);
+    }
+}
